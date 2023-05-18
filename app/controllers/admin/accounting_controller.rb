b6cc5cafc955878efbe3a37e45646ac85dedbfa2
devika <devikaskrishnan2018@gmail.com>
2023-05-18 14:56:16 +0530

    add accounting report directly if product purchased or sales report added

--- app/controllers/admin/accounting_controller.rb
+++ app/controllers/admin/accounting_controller.rb
@@ -39,23 +39,6 @@ class Admin::AccountingController < ApplicationController
   end
   
 
-  def edit
-    @accounting_record = Accounting.find(params[:id])
-    @orders = Order.all
-    @sales_reports = SalesReport.all
-  end
-  
-
-  def update
-    @accounting_record = Accounting.find(params[:id])
-    if @accounting_record.update(accounting_params)
-      flash[:success] = "Accounting record updated successfully!"
-      redirect_to admin_accounting_index_path
-    else
-      render :edit
-    end
-  end
-
   def destroy
     @accounting_record = Accounting.find(params[:id])
     @accounting_record.destroy
--- app/models/sales_report.rb
+++ app/models/sales_report.rb
@@ -1,6 +1,11 @@
 class SalesReport < ApplicationRecord
-    belongs_to :shop
-    belongs_to :category
-    has_one :accounting
-    validates :date_of_purchase, :weight, :total_price, :product_name, :price, :quantity, :category_id, presence: true
+  belongs_to :shop
+  belongs_to :category
+  has_one :accounting, dependent: :destroy
+  after_create :create_accounting
+  validates :date_of_purchase, :weight, :total_price, :product_name, :price, :quantity, :category_id, presence: true
+
+  def create_accounting
+    Accounting.create(sales_report: self)
   end
+end
