0de6e825a8b48653f4062245bb705d1dfe37c70a
devika <devikaskrishnan2018@gmail.com>
2023-05-19 15:24:59 +0530

    added image validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,13 +7,18 @@ class Product < ApplicationRecord
   has_many :carts, through: :cart_items
   has_many :reviews, dependent: :destroy
 
-  validates :product_title, presence: true, length: {minimum: 6, maximum: 30}
-  validates :description, presence: true, length: {minimum: 10, maximum: 3000}
+  validates :product_title, presence: true, length: { minimum: 6, maximum: 30 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 3000 }
   validates :sku_id, presence: true, uniqueness: true
   validates :weight, presence: true
+  validates :category_ids, presence: true
   validates :quantity, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
-  validates :image, presence: true
+  
   has_one_attached :image
+  validate :image_presence
 
+  def image_presence
+    errors.add(:image, "must be attached") unless image.attached?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_one :cart
   has_many :orders, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   def create_cart
     Cart.create(user: self)
   end
