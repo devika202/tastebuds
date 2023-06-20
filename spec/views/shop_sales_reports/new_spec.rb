631eac1922ac9a36b27d657e255cbd26dce3f604
DEVIKA S KRISHNAN <devikaskrishnan2018@gmail.com>
2023-06-20 13:52:21 +0530

    front end design break cleared

--- spec/views/shop_sales_reports/new_spec.rb
+++ spec/views/shop_sales_reports/new_spec.rb
@@ -11,38 +11,31 @@ RSpec.describe 'shop_sales_reports/new', type: :view do
     assert_select 'h1', text: 'Add Sales Report', count: 1
     assert_select 'form[action=?][method=?]', shop_sales_reports_path, 'post' do
       assert_select 'div.form-group.row.form-label' do
-        assert_select 'label.col-2.col-form-label.text-dark[for=?]', 'sales_report_product_name', text: /Product Name/i, count: 1
         assert_select 'div.col-10 input#sales_report_product_name.form-control.shadow.rounded[type=?][required=?][placeholder=?]', 'text', 'required', 'Enter Product name', count: 1
       end
 
       assert_select 'div.form-group.row.form-label' do
-        assert_select 'label.col-2.col-form-label.text-dark[for=?]', 'sales_report_price', text: /Price/i, count: 1
         assert_select 'div.col-10 input#sales_report_price.form-control.shadow.rounded[type=?][step=?][required=?][placeholder=?]', 'number', '0.01', 'required', 'Enter Product Price', count: 1
       end
 
       assert_select 'div.form-group.row.form-label' do
-        assert_select 'label.col-2.col-form-label.text-dark[for=?]', 'sales_report_weight', text: /Weight/i, count: 1
         assert_select 'div.col-5 input#sales_report_weight.form-control.shadow.rounded[type=?][placeholder=?]', 'number', 'Weight of Product purchased', count: 1
         assert_select 'div.col-5 select[name="sales_report[weight_unit]"].form-control.shadow.rounded', count: 1
       end
 
       assert_select 'div.form-group.row.form-label' do
-        assert_select 'label.col-2.col-form-label.text-dark[for=?]', 'sales_report_quantity', text: /Quantity/i, count: 1
         assert_select 'div.col-10 input#sales_report_quantity.form-control.shadow.rounded[type=?][required=?][placeholder=?]', 'number', 'required', 'No of products purchased', count: 1
       end
 
       assert_select 'div.form-group.row.form-label' do
-        assert_select 'label.col-2.col-form-label.text-dark[for=?]', 'sales_report_total_price', text: /Total Price/i, count: 1
         assert_select 'div.col-10 input#sales_report_total_price.form-control.shadow.rounded[type=?][step=?][required=?][placeholder=?]', 'number', '0.01', 'required', 'Total Price', count: 1
       end
 
       assert_select 'div.form-group.row.form-label' do
-        assert_select 'label.col-2.col-form-label.text-dark[for=?]', 'sales_report_date_of_purchase', text: /Date of Purchase/i, count: 1
         assert_select 'div.col-10 input#sales_report_date_of_purchase.form-control.shadow.rounded[type=?][required=?][max=?]', 'date', 'required', Date.today.to_s, count: 1
       end
 
       assert_select 'div.form-group.row.form-label' do
-        assert_select 'label.col-2.col-form-label.text-dark[for=?]', 'sales_report_category_id', text: /Category/i, count: 1
         assert_select 'div.col-10 select#sales_report_category_id.form-control.shadow.rounded[required="required"]' do
             assert_select 'option[value=?]', '', text: 'Select a category', count: 1
           end
